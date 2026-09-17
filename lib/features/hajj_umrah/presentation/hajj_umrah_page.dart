068de60b3e8cf11c47841d7b99126f3440bcbc59
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 02:53:45 +0300

    feat: establish MADAR app shell and feature entry points

---
+++ lib/features/hajj_umrah/presentation/hajj_umrah_page.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class HajjUmrahPage extends StatelessWidget {const HajjUmrahPage({super.key});@override Widget build(BuildContext c)=>Scaffold(appBar:AppBar(title:const Text('الحج والعمرة')),body:const Center(child:Text('الدليل المرحلي والمناسك والأذكار والمصادر ووضع المتابعة سيُبنى فوق حزمة محتوى موثقة.')));}}
