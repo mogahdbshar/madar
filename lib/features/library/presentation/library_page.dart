fccd50f08d3d3b058b3856948a63d8c480ba7171
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 02:53:47 +0300

    feat: establish MADAR app shell and feature entry points

---
+++ lib/features/library/presentation/library_page.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class LibraryPage extends StatelessWidget {const LibraryPage({super.key});@override Widget build(BuildContext c)=>Scaffold(appBar:AppBar(title:const Text('المكتبة الإسلامية')),body:const Center(child:Text('السيرة والعلوم والفقه والتاريخ والمقالات الموثوقة مع التصنيف والبحث والحفظ.')));}}
