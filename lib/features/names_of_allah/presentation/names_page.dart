ce58b16a19ee57d4894636bf622e072bc4b477bf
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 02:48:19 +0300

    feat: establish complete MADAR navigation and feature shells

---
+++ lib/features/names_of_allah/presentation/names_page.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class NamesPage extends StatelessWidget { const NamesPage({super.key}); @override Widget build(BuildContext c) => const Scaffold(body: Center(child: Text('أسماء الله الحسنى'))); }
