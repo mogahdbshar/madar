3b2fa60bfd215a4b54b85c9a79784323f97e2627
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 02:34:57 +0300

    feat: establish core architecture and feature boundaries

---
+++ lib/core/localization/app_strings.dart
@@ -0,0 +1,9 @@
+abstract final class AppStrings {
+  static const appName = 'مَدار';
+  static const appNameLatin = 'MADAR';
+  static const home = 'الرئيسية';
+  static const quran = 'القرآن';
+  static const hadith = 'الحديث';
+  static const worship = 'العبادة';
+  static const more = 'المزيد';
+}
