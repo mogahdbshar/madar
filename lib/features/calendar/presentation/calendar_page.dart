d6effffb61ef458290c8cc08a35ff8f97d5d4f85
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 02:48:16 +0300

    feat: establish complete MADAR navigation and feature shells

---
+++ lib/features/calendar/presentation/calendar_page.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class CalendarPage extends StatelessWidget { const CalendarPage({super.key}); @override Widget build(BuildContext c) => const Scaffold(body: Center(child: Text('التقويم'))); }
