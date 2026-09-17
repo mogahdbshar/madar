f3fecfcffecee3dd9628920205fbbea227efeae2
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 02:40:09 +0300

    feat: establish verified content and domain primitives

---
+++ lib/features/hajj_umrah/domain/ritual_step.dart
@@ -0,0 +1,13 @@
+class RitualStep {
+  const RitualStep({
+    required this.id,
+    required this.title,
+    required this.order,
+    required this.source,
+  });
+
+  final String id;
+  final String title;
+  final int order;
+  final String source;
+}
