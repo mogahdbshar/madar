91bbf12499f532ca9265e8f556fb3df01b9815f2
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 02:48:11 +0300

    feat: establish complete MADAR navigation and feature shells

---
+++ lib/features/prayer/presentation/prayer_page.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class PrayerPage extends StatelessWidget { const PrayerPage({super.key}); @override Widget build(BuildContext c) => const Scaffold(body: Center(child: Text('الصلاة'))); }
