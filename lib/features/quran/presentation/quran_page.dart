d4b7da6322d175b7aeef6a2b1037b4b135a19827
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 02:48:03 +0300

    feat: establish complete MADAR navigation and feature shells

---
+++ lib/features/quran/presentation/quran_page.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class QuranPage extends StatelessWidget { const QuranPage({super.key}); @override Widget build(BuildContext c) => const Scaffold(body: Center(child: Text('القرآن'))); }
