f2249f99dcee79230ef835d2ad60bd5e4812a94c
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 03:06:12 +0300

    fix: align domain models with Drift repositories

--- lib/features/worship_tracking/domain/worship_models.dart
+++ lib/features/worship_tracking/domain/worship_models.dart
@@ -1,9 +1,5 @@
 enum WorshipType { prayer, quran, adhkar, tasbeeh }
-
 class WorshipEntry {
-  const WorshipEntry({required this.id, required this.type, required this.date, required this.completed});
-  final String id;
-  final WorshipType type;
-  final DateTime date;
-  final bool completed;
+  const WorshipEntry({required this.id,required this.type,required this.date,required this.completed});
+  final String id; final WorshipType type; final DateTime date; final bool completed;
 }
