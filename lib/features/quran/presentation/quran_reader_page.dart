22488c07e5a0942b863a8d1ccaeb4f318fdc4cfe
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 04:20:30 +0300

    fix: resolve Quran repository provider import

--- lib/features/quran/presentation/quran_reader_page.dart
+++ lib/features/quran/presentation/quran_reader_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../core/database/database_provider.dart';
 import '../../../core/database/repository_providers.dart';
 import '../../../core/design_system/madar_ui.dart';
 import '../domain/quran_models.dart';
