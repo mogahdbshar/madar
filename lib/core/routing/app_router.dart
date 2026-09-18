3e23b79901036ce2286e3c37a2bf85a805a0606c
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 03:14:58 +0300

    feat: expand MADAR app functionality

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../features/home/presentation/home_page.dart';
@@ -14,23 +15,14 @@ import '../../features/library/presentation/library_page.dart';
 import '../../features/audio/presentation/audio_page.dart';
 import '../../features/search/presentation/search_page.dart';
 import '../../features/settings/presentation/settings_page.dart';
-
-final appRouterProvider = Provider<GoRouter>((ref) => GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(path: '/', builder: (_, __) => const HomePage()),
-    GoRoute(path: '/quran', builder: (_, __) => const QuranPage()),
-    GoRoute(path: '/hadith', builder: (_, __) => const HadithPage()),
-    GoRoute(path: '/adhkar', builder: (_, __) => const AdhkarPage()),
-    GoRoute(path: '/prayer', builder: (_, __) => const PrayerPage()),
-    GoRoute(path: '/qibla', builder: (_, __) => const QiblaPage()),
-    GoRoute(path: '/calendar', builder: (_, __) => const CalendarPage()),
-    GoRoute(path: '/names', builder: (_, __) => const NamesPage()),
-    GoRoute(path: '/tasbeeh', builder: (_, __) => const TasbeehPage()),
-    GoRoute(path: '/hajj-umrah', builder: (_, __) => const HajjUmrahPage()),
-    GoRoute(path: '/library', builder: (_, __) => const LibraryPage()),
-    GoRoute(path: '/audio', builder: (_, __) => const AudioPage()),
-    GoRoute(path: '/search', builder: (_, __) => const SearchPage()),
-    GoRoute(path: '/settings', builder: (_, __) => const SettingsPage()),
-  ],
-));
+import '../../features/worship_tracking/presentation/worship_tracking_page.dart';
+final appRouterProvider=Provider<GoRouter>((ref)=>GoRouter(initialLocation:'/',routes:[
+ GoRoute(path:'/',builder:(_,__)=>const HomePage()),GoRoute(path:'/quran',builder:(_,__)=>const QuranPage()),
+ GoRoute(path:'/hadith',builder:(_,__)=>const HadithPage()),GoRoute(path:'/adhkar',builder:(_,__)=>const AdhkarPage()),
+ GoRoute(path:'/prayer',builder:(_,__)=>const PrayerPage()),GoRoute(path:'/qibla',builder:(_,__)=>const QiblaPage()),
+ GoRoute(path:'/calendar',builder:(_,__)=>const CalendarPage()),GoRoute(path:'/names',builder:(_,__)=>const NamesPage()),
+ GoRoute(path:'/tasbeeh',builder:(_,__)=>const TasbeehPage()),GoRoute(path:'/hajj-umrah',builder:(_,__)=>const HajjUmrahPage()),
+ GoRoute(path:'/library',builder:(_,__)=>const LibraryPage()),GoRoute(path:'/audio',builder:(_,__)=>const AudioPage()),
+ GoRoute(path:'/search',builder:(_,__)=>const SearchPage()),GoRoute(path:'/settings',builder:(_,__)=>const SettingsPage()),
+ GoRoute(path:'/worship',builder:(_,__)=>const WorshipTrackingPage()),
+]));
