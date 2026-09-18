769980a5d6b85b44b715473cc9e11c3763d1f915
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 03:11:19 +0300

    feat: add persistent user data and local search

--- lib/core/database/database_provider.dart
+++ lib/core/database/database_provider.dart
@@ -1,9 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'app_database.dart';
-import 'drift_database.dart';
-
-final appDatabaseProvider = Provider<AppDatabase>((ref) {
-  final db = AppDatabase(openMadarDatabase());
-  ref.onDispose(db.close);
-  return db;
-});
+import 'app_database.dart';import 'drift_database.dart';import 'user_data_repository.dart';
+final appDatabaseProvider=Provider<AppDatabase>((ref){final db=AppDatabase(openMadarDatabase());ref.onDispose(db.close);return db;});
+final userDataRepositoryProvider=Provider<UserDataRepository>((ref)=>UserDataRepository(ref.watch(appDatabaseProvider)));
