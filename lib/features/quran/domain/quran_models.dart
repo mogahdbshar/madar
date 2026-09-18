a508207ffd99d9823a81b742d0366011c1fbefec
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 03:18:20 +0300

    feat: add Quran ayah model

--- lib/features/quran/domain/quran_models.dart
+++ lib/features/quran/domain/quran_models.dart
@@ -2,3 +2,5 @@ class QuranSurah {
   const QuranSurah({required this.number, required this.nameArabic, required this.nameLatin, required this.ayahCount});
   final int number; final String nameArabic; final String nameLatin; final int ayahCount;
 }
+
+class QuranAyah { const QuranAyah({required this.id,required this.surahNumber,required this.ayahNumber,required this.text}); final String id; final int surahNumber; final int ayahNumber; final String text; }
