c2911c3fd7061b42c802f403b73f38be291a0b8b
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 04:35:19 +0300

    fix: remove duplicate Quran repository provider import

--- lib/features/quran/presentation/quran_reader_page.dart
+++ lib/features/quran/presentation/quran_reader_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../core/database/repository_providers.dart';
-import '../../../core/database/database_provider.dart';
 
 import '../../../core/design_system/madar_ui.dart';
 import '../domain/quran_models.dart';
