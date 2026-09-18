753f84af9972104fced1da2ce4d70421fd74245d
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 03:17:49 +0300

    fix: normalize Arabic whitespace correctly

--- lib/core/content/search_normalizer.dart
+++ lib/core/content/search_normalizer.dart
@@ -1,5 +1,5 @@
 abstract final class SearchNormalizer {
   static String normalizeArabic(String value) {
-    return value.replaceAll(RegExp(r'[إأآٱ]'), 'ا').replaceAll('ى', 'ي').replaceAll('ة', 'ه').replaceAll(RegExp(r'[ًٌٍَُِّْـ]'), '').replaceAll(RegExp(r'[^\u0600-\u06FF\u0030-\u0039a-zA-Z ]'), ' ').replaceAll(RegExp(r'\\s+'), ' ').trim().toLowerCase();
+    return value.replaceAll(RegExp(r'[إأآٱ]'), 'ا').replaceAll('ى', 'ي').replaceAll('ة', 'ه').replaceAll(RegExp(r'[ًٌٍَُِّْـ]'), '').replaceAll(RegExp(r'[^\u0600-\u06FF\u0030-\u0039a-zA-Z ]'), ' ').replaceAll(RegExp(r'\s+'), ' ').trim().toLowerCase();
   }
 }
