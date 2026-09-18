fecd61982caf57d4204bca288c6b100be1842fb1
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 03:05:41 +0300

    feat: connect Drift repositories to domain

--- lib/core/database/database_provider.dart
+++ lib/core/database/database_provider.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:drift_flutter/drift_flutter.dart';
 import 'app_database.dart';
+import 'drift_database.dart';
 
-final databaseProvider = Provider<AppDatabase>((ref) {
-  final database = AppDatabase(driftDatabase(name: 'madar'));
-  ref.onDispose(database.close);
-  return database;
+final appDatabaseProvider = Provider<AppDatabase>((ref) {
+  final db = AppDatabase(openMadarDatabase());
+  ref.onDispose(db.close);
+  return db;
 });
