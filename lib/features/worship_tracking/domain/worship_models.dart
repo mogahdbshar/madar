893e5f8247044188c3087d071bdd7137ed68d4b1
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 03:15:04 +0300

    feat: expand MADAR app functionality

--- lib/features/worship_tracking/domain/worship_models.dart
+++ lib/features/worship_tracking/domain/worship_models.dart
@@ -1,5 +1,2 @@
-enum WorshipType { prayer, quran, adhkar, tasbeeh }
-class WorshipEntry {
-  const WorshipEntry({required this.id,required this.type,required this.date,required this.completed});
-  final String id; final WorshipType type; final DateTime date; final bool completed;
-}
+enum WorshipType { prayer,quran,adhkar,tasbeeh }
+class WorshipEntry { const WorshipEntry({required this.id,required this.type,required this.date,required this.completed}); final String id; final WorshipType type; final DateTime date; final bool completed; }
