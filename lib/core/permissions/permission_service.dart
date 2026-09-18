a45c62782556baabf7fed6f7b4487b299127a3c1
mogahdbshar <mogahdbshar@gmail.com>
2026-09-18 03:40:51 +0300

    feat: close prayer worship and notification foundations

--- lib/core/permissions/permission_service.dart
+++ lib/core/permissions/permission_service.dart
@@ -1,17 +1,7 @@
 import 'package:permission_handler/permission_handler.dart';
-
-class MadarPermissionService {
-  const MadarPermissionService();
-
-  Future<bool> requestNotifications() async {
-    final status = await Permission.notification.request();
-    return status.isGranted;
-  }
-
-  Future<bool> requestLocation() async {
-    final status = await Permission.locationWhenInUse.request();
-    return status.isGranted;
-  }
-
-  Future<bool> openSystemSettings() => openAppSettings();
+class MadarPermissionService{
+ const MadarPermissionService();
+ Future<bool> requestNotifications() async => (await Permission.notification.request()).isGranted;
+ Future<bool> requestLocation() async => (await Permission.locationWhenInUse.request()).isGranted;
+ Future<void> openSystemSettings()=>openAppSettings();
 }
